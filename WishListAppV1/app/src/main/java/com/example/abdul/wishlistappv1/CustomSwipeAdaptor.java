dde765a58b2aab3d2fba89187cc3ce93cd5c55a1
wsh1231 <1505600@my.brunel.ac.uk>
2016-12-02 02:20:41 +0000

    Added TV list to the app

---
+++ WishListAppV1/app/src/main/java/com/example/abdul/wishlistappv1/CustomSwipeAdaptor.java
@@ -0,0 +1,52 @@
+package com.example.abdul.wishlistappv1;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by william on 02/12/16.
+ */
+
+public class CustomSwipeAdaptor extends PagerAdapter {
+    private int[] image_resources = {R.drawable.fear_the_walking_dead,R.drawable.game_of_thrones,R.drawable.sons_of_anarchy,R.drawable.walking_dead};
+    private Context ctx;
+    private LayoutInflater layoutInflater;
+
+    public CustomSwipeAdaptor(Context ctx)
+    {
+        this.ctx = ctx;
+    }
+
+
+    @Override
+    public int getCount() {
+        return image_resources.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object o) {
+        return (view==(LinearLayout)o);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View item_view = layoutInflater.inflate(R.layout.swipe_layout,container,false);
+        ImageView imageView = (ImageView)item_view.findViewById(R.id.image_view);
+        imageView.setImageResource(image_resources[position]);
+        container.addView(item_view);
+
+        return item_view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position,Object object) {
+        container.removeView((LinearLayout)object);
+    }
+
+}
--- WishListAppV1/app/src/main/java/com/example/abdul/wishlistappv1/TVList.java
+++ WishListAppV1/app/src/main/java/com/example/abdul/wishlistappv1/TVList.java
@@ -1,13 +1,43 @@
 package com.example.abdul.wishlistappv1;
 
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class TVList extends AppCompatActivity {
+
+
+    ViewPager viewPager;
+    CustomSwipeAdaptor adaptor;
+
+    public Button homebutton;
+
+    public void homeButton(){
+        homebutton= (Button)findViewById(R.id.homeButton);
+        homebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(TVList.this,Menu.class);
+
+                startActivity(home);
+            }
+        });
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tvlist);
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        adaptor = new CustomSwipeAdaptor(this);
+        viewPager.setAdapter(adaptor);
+        homeButton();
     }
 }
