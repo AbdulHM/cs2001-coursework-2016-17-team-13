3f70555a43c5efb41c1cbb5484c9ca287ad76fde
wsh1231 <1505600@my.brunel.ac.uk>
2016-12-07 13:41:28 +0000

    Added the music list page

--- WishListAppV1/app/src/main/java/com/example/abdul/wishlistappv1/MusicList.java
+++ WishListAppV1/app/src/main/java/com/example/abdul/wishlistappv1/MusicList.java
@@ -1,13 +1,30 @@
 package com.example.abdul.wishlistappv1;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MusicList extends AppCompatActivity {
 
+    ViewPager viewPager;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_list);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        PagerAdapter adapter = new CustomAdapter (MusicList.this);
+        viewPager.setAdapter(adapter);
     }
+
+    //    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
 }
